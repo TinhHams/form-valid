bd53538d3ac5023f27d7740fae31ae7e7353e6c3
tinhhams <100270265+TinhHams@users.noreply.github.com>
2023-08-16 10:10:06 +0700

    Add files via upload

--- validator.js
+++ validator.js
@@ -1,9 +1,18 @@
 // Hàm Validator
 function Validator(options) {
 
+    var selectorRules = {};
+
     function Validate(inputElement, rule) {
-        var errorMessage = rule.test(inputElement.value);
-        var errorElement = inputElement.parentElement.querySelector(options.errorSelector)
+        var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+        var errorMessage;
+
+        var rules = selectorRules[rule.selector];
+
+        for (var i = 0; i < rules.length; ++i) {
+            errorMessage = rules[i](inputElement.value);
+            if (errorMessage) break;
+        } 
 
         if (errorMessage) {
             errorElement.innerText = errorMessage;
@@ -20,6 +29,12 @@ function Validator(options) {
     if (formElement) {
         
         options.rules.forEach(function (rule) {
+            if (Array.isArray(selectorRules[rule.selector])) {
+                selectorRules[rule.selector].push(rule.test);
+            } else {
+                selectorRules[rule.selector] = [rule.test];
+            }
+
             var inputElement = formElement.querySelector(rule.selector);
 
             if (inputElement) {
@@ -45,30 +60,40 @@ function Validator(options) {
 // Nguyên tắc của các rule:
 // 1. Khi có lỗi --> trả ra message lỗi
 // 2. Khi hợp lệ --> ko trả ra gì cả (undefined)
-Validator.isRequired = function (selector) {
+Validator.isRequired = function (selector, message) {
     return {
         selector: selector,
         test: function (value) {
-            return value.trim() ? undefined : "Vui lòng nhập trường này";
+            return value.trim() ? undefined : message || "Vui lòng nhập trường này";
         }
     }
 };
 
-Validator.isEmail = function (selector) {
+Validator.isEmail = function (selector, message) {
     return {
         selector: selector,
         test: function (value) {
             var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            return (regex.test(value)) ? undefined : "Trường này không phải là email";
+            return (regex.test(value)) ? undefined : message || "Trường này không phải là email";
         }
     }
 };
 
-Validator.minLength = function (selector, min) {
+Validator.minLength = function (selector, min, message) {
     return {
         selector: selector,
         test: function (value) {
-            return value.length >= min ? undefined : `Vui lòng nhập tối thiểu ${min} kí tự`;
+            return value.length >= min ? undefined : message || `Vui lòng nhập tối thiểu ${min} kí tự`;
         }
     }
 };
+
+Validator.isConfirmed = function (selector, confirmPassword, message) {
+    return {
+        selector: selector,
+        test: function(value) {
+            return value === confirmPassword() ? undefined : message || "Giá trị nhập vào không chính xác";
+        }
+    }
+
+}
